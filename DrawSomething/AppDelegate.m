03c40e1e0def765fd18ee3911f75b0f85fef161d
Neil Singh <Neil@neils-macbook-pro.local>
2016-05-22 03:29:37 -0500

    Initial Commit

---
+++ DrawSomething/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  DrawSomething
+//
+//  Created by Neil Singh on 5/22/16.
+//  Copyright © 2016 Neil Singh. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+@end
---
+++ DrawSomething/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  DrawSomething
+//
+//  Created by Neil Singh on 5/22/16.
+//  Copyright © 2016 Neil Singh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
